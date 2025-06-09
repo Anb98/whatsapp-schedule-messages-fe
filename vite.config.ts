3dc6a4e399c62be31d613d8a3acb49ba5eec7ce7
Anb98 <aabdiel1998@gmail.com>
2025-06-09 12:34:14 -0600

    ✨ add @types/node dependency for improved TypeScript support

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+const port = process.env.PORT || 3000;
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  preview: {
+    port: Number(port),
+    host: true, // This enables listening on all network interfaces
+  },
+  server: {
+    // Also add this for development server
+    host: true, // This enables listening on all network interfaces
+    port: Number(port),
+  },
+});
